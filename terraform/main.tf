a58a3a880a3a2ef97e5441716f3e5629c009f2b4
jkim-mlops <josephykim317@gmail.com>
2026-09-18 20:16:36 -0400

    feat(main.tf): :sparkles: setup terraform with project

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.16"
+
+  backend "gcs" {
+    bucket = "jk-controlled-burn-dev-tfstate"
+    prefix = "dev"
+  }
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 8.3"
+    }
+  }
+}
+
+provider "google" {
+  project = "jk-controlled-burn-dev"
+  region  = "us-east4"
+}
